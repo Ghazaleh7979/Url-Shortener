3ebbc78c0967657ca07cfd08a8103adb15cb1445
ghazaleh7979 <masoome.aliyannejhadi@gmail.com>
2023-02-09 01:45:17 +0330

    nn

---
+++ Migrations/20230208221454_nn.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class nn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "UserPass");
+
+            migrationBuilder.CreateTable(
+                name: "KeyList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Key = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyList", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeyList");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "UserPass",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
